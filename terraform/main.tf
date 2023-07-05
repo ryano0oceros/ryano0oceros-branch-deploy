5f86b877d1fb610ad501f3ee0c17624131be5411
Ryan Fallon <ryano0oceros@github.com>
2023-07-05 13:27:24 -0400

    add vpc module call

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,35 @@
-# provider "aws" {
-#   region = local.region
-# }
+provider "aws" {
+  region = local.region
+}
 
-# data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {}
 
-# locals {
-#   name   = "ex-${basename(path.cwd)}"
-#   region = "us-east-1"
+locals {
+  name   = "ex-${basename(path.cwd)}"
+  region = "us-east-1"
 
-#   vpc_cidr = "10.24.0.0/16"
-#   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.24.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-#   tags = {
-#     Example    = local.name
-#     GithubRepo = "ryano0oceros-at-318-tf-aws-vpc"
-#     GithubOrg  = "ryano0oceros"
-#   }
-# }
+  tags = {
+    Example    = local.name
+    GithubRepo = "ryano0oceros-at-318-tf-aws-vpc"
+    GithubOrg  = "ryano0oceros"
+  }
+}
 
-# ################################################################################
-# ## VPC Module
-# ################################################################################
+################################################################################
+## VPC Module
+################################################################################
 
-# module "vpc" {
-#   source = "git::https://github.com/ryano0oceros/ryano0oceros-at-318-tf-aws-vpc?ref=e1f1033"
+module "vpc" {
+  source = "git::https://github.com/ryano0oceros/ryano0oceros-at-318-tf-aws-vpc?ref=e1f1033"
 
-#   name = local.name
-#   cidr = local.vpc_cidr
+  name = local.name
+  cidr = local.vpc_cidr
 
-#   azs             = local.azs
-#   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
